a59de042547c2f72ac3ad5cf28f6647da7a2ea30
Juan Jose Salvi <salvijuan60@gmail.com>
2026-01-04 02:00:04 -0300

    Se hizo un arreglo en la rama feat/registro-usuarios ya que esta no mandaba los datos al repositorio remoto

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { register, login } from "../controllers/usuariosController.js";
 import { validarToken } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
-// router.post("/:id", validarToken, agregarProductoCarrito);
-// router.delete("/:id", validarToken, eliminarProductoCarrito);
-// router.get("/", validarToken, obtenerDatosCarrito);
+router.post("/register", register);
+router.post("/login", login);
+
 export default router;
