953e9bf3d9692d5c06f3e9d710eb3b78b67e513f
Juan Jose Salvi <salvijuan60@gmail.com>
2026-01-05 11:49:17 -0300

    feat(carrito):Se vinculo el id del usuario con el id del carrito

---
+++ src/models/carritoModel.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const carritoSchema = new Schema(
+  {
+    usuarioId: {
+      type: Schema.Types.ObjectId,
+      ref: "usuarios",
+      required: true,
+      unique: true,
+    },
+
+    juegos: [
+      {
+        juegoId: {
+          type: Schema.Types.ObjectId,
+          ref: "juegos",
+          required: true,
+        },
+
+        titulo: {
+          type: String,
+          required: true,
+        },
+
+        precio: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+
+        imagenPortada: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    total: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    activo: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+carritoSchema.pre("save", function () {
+  this.total = this.juegos.reduce((acc, juego) => acc + juego.precio, 0);
+});
+
+export const carritoModel = mongoose.model("carritos", carritoSchema);
--- src/services/usuariosServices.js
+++ src/services/usuariosServices.js
@@ -1,8 +1,7 @@
 import { usuarioModel } from "../models/usuariosModel.js";
-
+import { carritoModel } from "../models/carritoModel.js";
 export const obtenerUsuariosServices = async () => {
   try {
-
     const usuarios = await usuarioModel.find();
 
     return {
@@ -39,6 +38,13 @@ export const register = async (nuevoUsuario) => {
 
     const usuarioDB = new usuarioModel(nuevoUsuario);
     await usuarioDB.save();
+
+    const carrito = new carritoModel({
+      usuarioId: usuarioDB._id,
+      juegos: [],
+      total: 0,
+    });
+    await carrito.save();
 
     return {
       json: {
@@ -83,3 +89,60 @@ export const obtenerUnUsuarioServices = async (id) => {
   }
 };
 
+export const editarUsuarioServices = async (id, datos) => {
+  try {
+    const usuario = await usuarioModel.findByIdAndUpdate(id, datos, {
+      new: true,
+    });
+
+    if (!usuario) {
+      return {
+        json: { message: "El usuario no existe" },
+        statusCode: 404,
+      };
+    }
+
+    return {
+      json: {
+        message: "Usuario actualizado exitosamente",
+        datos: usuario,
+      },
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error("Error al editar usuario:", error);
+    return {
+      json: { message: "Error interno del servidor: " + error.message },
+      statusCode: 500,
+    };
+  }
+};
+
+export const borrarUsuarioServices = async (id) => {
+  try {
+    const usuarioEliminado = await usuarioModel.findByIdAndDelete(id);
+
+    if (!usuarioEliminado) {
+      return {
+        json: {
+          message: "El usuario no existe",
+        },
+        statusCode: 404,
+      };
+    }
+
+    return {
+      json: {
+        message: "Se elimino el usuario exitosamente!!",
+        datos: usuarioEliminado,
+      },
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error("Error al eliminar usuario", error);
+    return {
+      json: { message: "Error interno del servidor: " + error.message },
+      statusCode: 500,
+    };
+  }
+};
