490719e83130ca95991df56f98c57fa178cc3e0f
Diaz Martin <elmartind17@gmail.com>
2026-02-13 17:01:48 -0300

    Fix(Server): Prueba de arreglar el servidor 3.

--- src/config/FireBase.js
+++ src/config/FireBase.js
@@ -1,9 +1,19 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-
-const serviceAccount = require("./key.json");
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key: process.env.PRIVATE_KEY
+    ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 try {
   admin.initializeApp({
